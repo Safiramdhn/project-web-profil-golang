956dd8385b197cef3e522fe9a60a86b107ed1566
Dewi Safira Ramadhani <safiramadhani9@gmail.com>
2024-10-12 16:56:03 +0800

    Add project.json

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 // Function to render project list
 const renderProjectList = (projects) => {
-    const contentDiv = document.getElementById('project-list');
-    contentDiv.innerHTML = '';
-  
-    if (projects && projects.length) {
-      projects.forEach((project) => {
-        const article = document.createElement('article');
-        article.innerHTML = `
+  const contentDiv = document.getElementById('project-list');
+  contentDiv.innerHTML = '';
+
+  if (projects && projects.length) {
+    projects.forEach((project) => {
+      const article = document.createElement('article');
+      article.innerHTML = `
             <hgroup>
               <h3>${project.name}</h3>
               <p>${project.start_date} - ${project.end_date} | <a href="${project.link}">Source Code</a></p>
@@ -14,45 +14,42 @@ const renderProjectList = (projects) => {
             <p class="description">Project Description: ${project.description}</p>
             <p class="tech-stack">Tech Stack: ${project.tech_stack}</p>
           `;
-        contentDiv.appendChild(article);
-      });
-    } else {
-      console.log('No projects found.');
-    }
-  };
-  
-  // Main function to load project data and render the list
-  const loadProject = () => {
-    const projects = [
-      {
-        id: 1,
-        name: 'Zettacamp Mini Project',
-        description: '',
-        start_date: 'dd/mm/yyyy',
-        end_date: 'dd/mm/yyyy',
-        tech_stack: '',
-        link: '',
-      },
-      {
-        id: 2,
-        name: 'Restaurant Management Project',
-        description: '',
-        start_date: 'dd/mm/yyyy',
-        end_date: 'dd/mm/yyyy',
-        tech_stack: '',
-        link: '',
-      },
-      {
-        id: 3,
-        name: 'Image Processing App Project',
-        description: '',
-        start_date: 'dd/mm/yyyy',
-        end_date: 'dd/mm/yyyy',
-        tech_stack: '',
-        link: '',
-      },
-    ];
-    renderProjectList(projects);
-  };
-  
-  document.addEventListener('DOMContentLoaded', loadProject);
+      contentDiv.appendChild(article);
+    });
+  } else {
+    console.log('No projects found.');
+  }
+};
+
+// Main function to load project data and render the list
+const loadProject = async () => {
+  fetch('assets/projects.json')
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(res.status);
+      }
+      return res.json();
+    })
+    .then((json) => {
+      const projects = json.projects;
+      renderProjectList(projects);
+    })
+    .catch((e) => {
+      throw new Error(e);
+    });
+};
+
+document.addEventListener('DOMContentLoaded', loadProject);
+
+// function to download resume pdf
+document.getElementById('download-resume').addEventListener('click', () => {
+  fetch('assets/Resume - Dewi Safira Ramadhani, S.Kom.pdf')
+    .then((res) => res.blob())
+    .then((blob) => {
+      const url = window.URL.createObjectURL(blob);
+      const a = document.getElementById('download-resume');
+      a.href = url;
+      a.download = 'Resume - Dewi Safira Ramadhani, S.Kom.pdf';
+      window.URL.revokeObjectURL(url);
+    });
+});
